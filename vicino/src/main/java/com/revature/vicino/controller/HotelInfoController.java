a2cc1095029455b6014728acf54b58146b73070e
unknown <sdanner08@yahoo.com>
2021-10-18 17:12:48 -0400

    validation check

--- vicino/src/main/java/com/revature/vicino/controller/HotelInfoController.java
+++ vicino/src/main/java/com/revature/vicino/controller/HotelInfoController.java
@@ -15,6 +15,7 @@ import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
+import java.util.Date;
 
 @RestController("bookingController")
 public class HotelInfoController{
@@ -24,7 +25,19 @@ public class HotelInfoController{
     @PostMapping()
     public Mono<String> getInfoLL(@RequestBody HotelRequest hotelRequest) throws IOException, UnirestException {
         ///check that values are not empty
+        if( hotelRequest.getNumOfRooms() == null || hotelRequest.getNumOfNights()==null || hotelRequest.getAdults()==null ||
+            hotelRequest.getLongitude() ==null || hotelRequest.getLatitude()==null || hotelRequest.getCheckinDay()==null ||
+            hotelRequest.getCheckinYear()==null || hotelRequest.getCheckinMonth()==null){
+            return Mono.just("You must fill out all required fields to receive hotel info.");
+        }
+        //There is a check for date already
+        //If you enter a date before today it'll respond with a "BadRequestException","message":"The checkin time is before today in accommodation {date}
+
+
+        //Logs the request
         log.info("Request {}", hotelRequest);
+
+        //Makes the request to the API
         HttpResponse<String> response = Unirest.get("https://travel-advisor.p.rapidapi.com/hotels/list-by-latlng?latitude="
                 +hotelRequest.getLatitude()+
                 "&longitude="+hotelRequest.getLongitude()+"&lang=en_US&hotel_class=1%2C2%2C3&limit=30&" +
@@ -39,7 +52,10 @@ public class HotelInfoController{
                 .header("x-rapidapi-key", "a3582833c4mshe7ab93b1542c50bp186450jsn34d6409641ab")
                 .asString();
 
+        //Logs the response from API
         log.info("Response {}", response.getBody());
+
+        //Returns the response to the front end
         return Mono.just(response.getBody());
     }
 
