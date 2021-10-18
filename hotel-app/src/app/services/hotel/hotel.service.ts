16067008ac6bbed51061224e62bfdfd4392f4dda
ctrumbo <camerontrumbo@gmail.com>
2021-10-18 16:12:12 -0400

    Added stuff

--- hotel-app/src/app/services/hotel/hotel.service.ts
+++ hotel-app/src/app/services/hotel/hotel.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,12 @@ export class HotelService {
     return this.httpCli.get<any>(`http://localhost:9090/location/${cityName}`, {withCredentials: true});
   }
 
-  getHotelInfo(latitude: number, longitude: number, adults: number, numOfRooms: number, numOfNights: number, checkinYear: number, checkinMonth: number, checkinDay: number) : Observable<any> {
+  getHotelInfo(latitude: number, longitude: number, adults: number, numOfRooms: number, numOfNights: number, checkinYear: number, checkinMonth: number, checkinDay: number, toDate: NgbDate) : Observable<any> {
+    if (toDate != null)
+      numOfNights = Math.floor(Date.UTC(checkinYear, checkinMonth, checkinDay) - Date.UTC(toDate.year, toDate.month, toDate.day))  / (1000 * 60 * 60 * 24);
+    else 
+      numOfNights = 1;
+    console.log(numOfNights);
     return this.httpCli.post<any>(`http://localhost:9090/booking`, {
       latitude: latitude,
       longitude: longitude,
